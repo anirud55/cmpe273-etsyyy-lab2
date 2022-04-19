e2c035a3b45528ad267be1e30bba2bd0973bca53
anirud55 <90978843+anirud55@users.noreply.github.com>
2022-04-18 17:29:09 -0700

    Update index.js

--- kafka-backend/index.js
+++ kafka-backend/index.js
@@ -30,11 +30,11 @@ mongoose.Promise = global.Promise;
 //Init Middleware
 app.use(express.json({extended:false}))
 
-app.use('/api/users',require('./src/routes/user.routes'))
-app.use('/api/shop',require('./src/routes/seller.routes'))
-app.use('/api/dashboard',require('./src/routes/dashboard.routes'))
-app.use('/api/order',require('./src/routes/order.routes'))
-app.use('/api/products',require('./src/routes/products.routes'))
+app.use('/api/users',require('./src/routes/user'))
+app.use('/api/shop',require('./src/routes/shop'))
+app.use('/api/dashboard',require('./src/routes/landingpage'))
+app.use('/api/order',require('./src/routes/order'))
+app.use('/api/products',require('./src/routes/products'))
 
 const PORT = process.env.PORT || 3002
 
